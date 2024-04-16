829eb351e4551ea0113b5d23bf5d0f47b2078537
HectorCalcinesGonzalez <159430801+HectorCalcinesGonzalez@users.noreply.github.com>
2024-04-16 12:44:03 +0100

    feat(Restaurante): atributos

---
+++ src/net/salesianos/restaurantes/Restaurante.java
@@ -0,0 +1,8 @@
+package src.net.salesianos.restaurantes;
+
+public class Restaurante {
+    private String nombre;
+    private String localizacion;
+    private int horario;
+    private float puntuacion;
+}
