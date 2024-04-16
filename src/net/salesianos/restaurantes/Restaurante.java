a6e9bb06d6f815b28176c31506b8cbf3309c408c
HectorCalcinesGonzalez <159430801+HectorCalcinesGonzalez@users.noreply.github.com>
2024-04-16 12:49:02 +0100

    feat(Restaurante): constructor  y getters and setters

--- src/net/salesianos/restaurantes/Restaurante.java
+++ src/net/salesianos/restaurantes/Restaurante.java
@@ -5,4 +5,44 @@ public class Restaurante {
     private String localizacion;
     private int horario;
     private float puntuacion;
+
+    public Restaurante(String nombre, String localizacion, int horario, float puntuacion) {
+        this.nombre = nombre;
+        this.localizacion = localizacion;
+        this.horario = horario;
+        this.puntuacion = puntuacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        this.localizacion = localizacion;
+    }
+
+    public int getHorario() {
+        return horario;
+    }
+
+    public void setHorario(int horario) {
+        this.horario = horario;
+    }
+
+    public float getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(float puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
 }
