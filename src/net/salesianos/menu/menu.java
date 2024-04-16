1b21e09186ca3968d9191e2b34cbf4b66197bc58
HectorCalcinesGonzalez <159430801+HectorCalcinesGonzalez@users.noreply.github.com>
2024-04-16 13:05:09 +0100

    feat(menu): menu creado

---
+++ src/net/salesianos/menu/menu.java
@@ -0,0 +1,15 @@
+package src.net.salesianos.menu;
+
+public class menu {
+    public void mostrarMenu() {
+        String menu = """
+                Menú de publicaciones:
+                1. Añadir restaurante
+                2. Editar restaurante
+                3. Mostrar restaurantes
+                4. Eliminar restaurante
+                Q. Salir del programa
+                """;
+        System.out.println(menu);
+    }
+}
