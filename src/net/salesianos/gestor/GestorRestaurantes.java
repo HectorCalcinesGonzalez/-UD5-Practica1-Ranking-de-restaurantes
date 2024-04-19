b4f0a32704e4bc0c1cdb1a9a56412c0d39e3703b
HectorCalcinesGonzalez <159430801+HectorCalcinesGonzalez@users.noreply.github.com>
2024-04-19 11:41:30 +0100

    refactor: cambiado nombre de la clase metodos

--- src/net/salesianos/gestor/GestorRestaurantes.java
+++ src/net/salesianos/gestor/GestorRestaurantes.java
@@ -1,26 +1,26 @@
-package src.net.salesianos.metodos;
+package src.net.salesianos.gestor;
 
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 import src.net.salesianos.restaurantes.*;
 
-public class Metodos {
-    private ArrayList<Restaurante> listaRestaurante;
+public class GestorRestaurantes {
+    private static ArrayList<Restaurante> listaRestaurante;
 
-    public Metodos() {
-        this.listaRestaurante = new ArrayList<>();
+    public GestorRestaurantes() {
+        listaRestaurante = new ArrayList<>();
     }
 
-    public void aniadirRestaurante(Restaurante nuevoRestaurante) {
+    public static void aniadirRestaurante(Restaurante nuevoRestaurante) {
         listaRestaurante.add(nuevoRestaurante);
     }
 
-    public void eliminarRestaurante(int indiceEmpleado) {
+    public static void eliminarRestaurante(int indiceEmpleado) {
         listaRestaurante.remove(indiceEmpleado);
     }
 
-    public void mostrarRestaurante() {
+    public static void mostrarRestaurante() {
         for (int i = 0; i < listaRestaurante.size(); i++) {
             if (listaRestaurante.get(i) != null) {
                 System.out.println(listaRestaurante.get(i).toString());
@@ -28,14 +28,16 @@ public class Metodos {
         }
     }
 
-    public void editarRestaurante(int indice, String nombre, String localizacion, String horario, int puntuacion) {
+    public static void editarRestaurante(int indice, String nombre, String localizacion, String horario,
+            float puntuacion) {
         if (indice >= 0 && indice < listaRestaurante.size()) {
             Restaurante restaurante = listaRestaurante.get(indice);
             restaurante.setNombre(nombre);
             restaurante.setLocalizacion(localizacion);
             restaurante.setHorario(horario);
             restaurante.setPuntuacion(puntuacion);
-            JOptionPane.showMessageDialog(null, "Restaurante editado correctamente.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Restaurante editado correctamente.", "Éxito",
+                    JOptionPane.INFORMATION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, "Índice de restaurante inválido.", "Error", JOptionPane.ERROR_MESSAGE);
         }
